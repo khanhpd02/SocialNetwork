405dc6acf2a64d04ad03d1f84d55498a586f20de
TruongTNH <dev.truongtran@gmail.com>
2024-03-09 17:06:40 +0700

    [update] -gendb

---
+++ SocialNetwork/Entity/AdministrativeRegion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialNetwork.Entity;
+
+public partial class AdministrativeRegion
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string NameEn { get; set; }
+
+    public string CodeName { get; set; }
+
+    public string CodeNameEn { get; set; }
+
+    public virtual ICollection<Province> Provinces { get; set; } = new List<Province>();
+}
--- SocialNetwork/Entity/SocialNetworkContext.cs
+++ SocialNetwork/Entity/SocialNetworkContext.cs
@@ -15,9 +15,15 @@ public partial class SocialNetworkContext : DbContext
     {
     }
 
+    public virtual DbSet<AdministrativeRegion> AdministrativeRegions { get; set; }
+
+    public virtual DbSet<AdministrativeUnit> AdministrativeUnits { get; set; }
+
     public virtual DbSet<Chat> Chats { get; set; }
 
     public virtual DbSet<Comment> Comments { get; set; }
+
+    public virtual DbSet<District> Districts { get; set; }
 
     public virtual DbSet<Friend> Friends { get; set; }
 
@@ -39,6 +45,8 @@ public partial class SocialNetworkContext : DbContext
 
     public virtual DbSet<Post> Posts { get; set; }
 
+    public virtual DbSet<Province> Provinces { get; set; }
+
     public virtual DbSet<RefreshToken> RefreshTokens { get; set; }
 
     public virtual DbSet<Report> Reports { get; set; }
@@ -59,12 +67,68 @@ public partial class SocialNetworkContext : DbContext
 
     public virtual DbSet<Video> Videos { get; set; }
 
+    public virtual DbSet<Ward> Wards { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=sql.bsite.net\\MSSQL2016;Initial Catalog=truongnetwwork_SocialNetwork;uid=truongnetwwork_SocialNetwork;pwd=social;MultipleActiveResultSets=True;TrustServerCertificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<AdministrativeRegion>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("administrative_regions_pkey");
+
+            entity.ToTable("administrative_regions");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.CodeName)
+                .HasMaxLength(255)
+                .HasColumnName("code_name");
+            entity.Property(e => e.CodeNameEn)
+                .HasMaxLength(255)
+                .HasColumnName("code_name_en");
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.NameEn)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnName("name_en");
+        });
+
+        modelBuilder.Entity<AdministrativeUnit>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("administrative_units_pkey");
+
+            entity.ToTable("administrative_units");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+            entity.Property(e => e.CodeName)
+                .HasMaxLength(255)
+                .HasColumnName("code_name");
+            entity.Property(e => e.CodeNameEn)
+                .HasMaxLength(255)
+                .HasColumnName("code_name_en");
+            entity.Property(e => e.FullName)
+                .HasMaxLength(255)
+                .HasColumnName("full_name");
+            entity.Property(e => e.FullNameEn)
+                .HasMaxLength(255)
+                .HasColumnName("full_name_en");
+            entity.Property(e => e.ShortName)
+                .HasMaxLength(255)
+                .HasColumnName("short_name");
+            entity.Property(e => e.ShortNameEn)
+                .HasMaxLength(255)
+                .HasColumnName("short_name_en");
+        });
+
         modelBuilder.Entity<Chat>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PK_Chat_1");
@@ -94,6 +158,49 @@ public partial class SocialNetworkContext : DbContext
                 .HasForeignKey(d => d.UserId)
                 .OnDelete(DeleteBehavior.Cascade)
                 .HasConstraintName("FK_Comment_User");
+        });
+
+        modelBuilder.Entity<District>(entity =>
+        {
+            entity.HasKey(e => e.Code).HasName("districts_pkey");
+
+            entity.ToTable("districts");
+
+            entity.HasIndex(e => e.ProvinceCode, "idx_districts_province");
+
+            entity.HasIndex(e => e.AdministrativeUnitId, "idx_districts_unit");
+
+            entity.Property(e => e.Code)
+                .HasMaxLength(20)
+                .HasColumnName("code");
+            entity.Property(e => e.AdministrativeUnitId).HasColumnName("administrative_unit_id");
+            entity.Property(e => e.CodeName)
+                .HasMaxLength(255)
+                .HasColumnName("code_name");
+            entity.Property(e => e.FullName)
+                .HasMaxLength(255)
+                .HasColumnName("full_name");
+            entity.Property(e => e.FullNameEn)
+                .HasMaxLength(255)
+                .HasColumnName("full_name_en");
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.NameEn)
+                .HasMaxLength(255)
+                .HasColumnName("name_en");
+            entity.Property(e => e.ProvinceCode)
+                .HasMaxLength(20)
+                .HasColumnName("province_code");
+
+            entity.HasOne(d => d.AdministrativeUnit).WithMany(p => p.Districts)
+                .HasForeignKey(d => d.AdministrativeUnitId)
+                .HasConstraintName("districts_administrative_unit_id_fkey");
+
+            entity.HasOne(d => d.ProvinceCodeNavigation).WithMany(p => p.Districts)
+                .HasForeignKey(d => d.ProvinceCode)
+                .HasConstraintName("districts_province_code_fkey");
         });
 
         modelBuilder.Entity<Friend>(entity =>
@@ -223,6 +330,48 @@ public partial class SocialNetworkContext : DbContext
             entity.HasOne(d => d.User).WithMany(p => p.Posts)
                 .HasForeignKey(d => d.UserId)
                 .HasConstraintName("FK_Post_User");
+        });
+
+        modelBuilder.Entity<Province>(entity =>
+        {
+            entity.HasKey(e => e.Code).HasName("provinces_pkey");
+
+            entity.ToTable("provinces");
+
+            entity.HasIndex(e => e.AdministrativeRegionId, "idx_provinces_region");
+
+            entity.HasIndex(e => e.AdministrativeUnitId, "idx_provinces_unit");
+
+            entity.Property(e => e.Code)
+                .HasMaxLength(20)
+                .HasColumnName("code");
+            entity.Property(e => e.AdministrativeRegionId).HasColumnName("administrative_region_id");
+            entity.Property(e => e.AdministrativeUnitId).HasColumnName("administrative_unit_id");
+            entity.Property(e => e.CodeName)
+                .HasMaxLength(255)
+                .HasColumnName("code_name");
+            entity.Property(e => e.FullName)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnName("full_name");
+            entity.Property(e => e.FullNameEn)
+                .HasMaxLength(255)
+                .HasColumnName("full_name_en");
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.NameEn)
+                .HasMaxLength(255)
+                .HasColumnName("name_en");
+
+            entity.HasOne(d => d.AdministrativeRegion).WithMany(p => p.Provinces)
+                .HasForeignKey(d => d.AdministrativeRegionId)
+                .HasConstraintName("provinces_administrative_region_id_fkey");
+
+            entity.HasOne(d => d.AdministrativeUnit).WithMany(p => p.Provinces)
+                .HasForeignKey(d => d.AdministrativeUnitId)
+                .HasConstraintName("provinces_administrative_unit_id_fkey");
         });
 
         modelBuilder.Entity<RefreshToken>(entity =>
@@ -376,6 +525,49 @@ public partial class SocialNetworkContext : DbContext
                 .HasForeignKey(d => d.PostId)
                 .OnDelete(DeleteBehavior.Cascade)
                 .HasConstraintName("FK_Video_Post");
+        });
+
+        modelBuilder.Entity<Ward>(entity =>
+        {
+            entity.HasKey(e => e.Code).HasName("wards_pkey");
+
+            entity.ToTable("wards");
+
+            entity.HasIndex(e => e.DistrictCode, "idx_wards_district");
+
+            entity.HasIndex(e => e.AdministrativeUnitId, "idx_wards_unit");
+
+            entity.Property(e => e.Code)
+                .HasMaxLength(20)
+                .HasColumnName("code");
+            entity.Property(e => e.AdministrativeUnitId).HasColumnName("administrative_unit_id");
+            entity.Property(e => e.CodeName)
+                .HasMaxLength(255)
+                .HasColumnName("code_name");
+            entity.Property(e => e.DistrictCode)
+                .HasMaxLength(20)
+                .HasColumnName("district_code");
+            entity.Property(e => e.FullName)
+                .HasMaxLength(255)
+                .HasColumnName("full_name");
+            entity.Property(e => e.FullNameEn)
+                .HasMaxLength(255)
+                .HasColumnName("full_name_en");
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnName("name");
+            entity.Property(e => e.NameEn)
+                .HasMaxLength(255)
+                .HasColumnName("name_en");
+
+            entity.HasOne(d => d.AdministrativeUnit).WithMany(p => p.Wards)
+                .HasForeignKey(d => d.AdministrativeUnitId)
+                .HasConstraintName("wards_administrative_unit_id_fkey");
+
+            entity.HasOne(d => d.DistrictCodeNavigation).WithMany(p => p.Wards)
+                .HasForeignKey(d => d.DistrictCode)
+                .HasConstraintName("wards_district_code_fkey");
         });
 
         OnModelCreatingPartial(modelBuilder);
