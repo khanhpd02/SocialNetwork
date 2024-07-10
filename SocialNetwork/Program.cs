f2070681b59c2112327c882d5d9d0fb6b5990a5c
TruongTNH <dev.truongtran@gmail.com>
2024-07-10 19:57:28 +0700

    oki code

--- SocialNetwork/Program.cs
+++ SocialNetwork/Program.cs
@@ -72,7 +72,7 @@ builder.Services.AddSwaggerGen(option =>
     {
         options.AddPolicy("CorsPolicy",
             builder => builder
-                .WithOrigins("https://font-socialk-network.vercel.app", "https://front-tlcn.vercel.app", "http://localhost:3000", "http://localhost:8081", "https://front-social-network-zeta.vercel.app") // Add your frontend origin(s) here
+                .WithOrigins("https://font-socialk-network.vercel.app", "https://front-tlcn.vercel.app", "http://localhost:3000", "http://localhost:8081", "https://front-social-network-zeta.vercel.app", "https://socialnetworkadmin.vercel.app") // Add your frontend origin(s) here
                 .AllowAnyMethod()
                 .AllowAnyHeader()
                 .AllowCredentials());
